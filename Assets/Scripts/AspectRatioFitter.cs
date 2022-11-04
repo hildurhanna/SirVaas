22b516ea2ee7c971ff1cb060e7346d8a4fb9fa1a
hildurhanna <112477585+hildurhanna@users.noreply.github.com>
2022-11-04 14:30:25 +0100

    Added AspectRatioFitter Script

---
+++ Assets/Scripts/AspectRatioFitter.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Camera))]
+[ExecuteAlways]
+public class AspectRatioFitter : MonoBehaviour
+{
+    
+    void Update()
+    {
+        Camera camera = gameObject.GetComponent<Camera>();
+        if (IsInLandscapeMode())
+        {
+            camera.orthographicSize = 4.5f;
+        }
+        else
+        {
+            camera.orthographicSize = 4.5f * Screen.height / Screen.width;
+        }
+    }
+    
+
+    bool IsInLandscapeMode() => Screen.width >= Screen.height;
+
+
+}
